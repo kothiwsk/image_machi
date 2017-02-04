f57c4eb7db401619f168e2e416c27228e1ec44b1
marellc <marella.chandan@gmail.com>
2017-02-03 21:58:01 -0600

    Window for full train directory images

--- src/sliding-window/sliding_window.py
+++ src/sliding-window/sliding_window.py
@@ -6,31 +6,52 @@ from pyimagesearch.helpers import sliding_window
 import argparse
 import time
 import cv2
+from os import listdir,walk
+from os.path import isfile, join
+import numpy
 
 # construct the argument parser and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True, help="Path to the image")
-args = vars(ap.parse_args())
+#ap = argparse.ArgumentParser()
+#ap.add_argument("-i", "--image", required=True, help="Path to the image")
+#args = vars(ap.parse_args())
+
+
+path = 'D:/Kaggle/comprtition_sealife_conservation/train'
+#path1 = 'D:/Kaggle/comprtition_sealife_conservation/train/ALB'
+lst = []
+for (dirpath, dirnames, filenames) in walk(path):
+	lst.extend(dirnames)
+#print lst
+for each in lst:
+	paths = path + "/" + each
+	print paths
+	files = [f for f in listdir(paths) if isfile(join(paths,f))]
+#image = numpy.empty(len(files), dtype=object)
+	image = []
+	for n in files:
+		image.append(cv2.imread(join(paths,n)))
+
 
 # load the image and define the window width and height
-image = cv2.imread(args["image"])
+#image = cv2.imread(args["image"])
 (winW, winH) = (128, 128)
 
 # loop over the image pyramid
-for resized in pyramid(image, scale=1.5):
-	# loop over the sliding window for each layer of the pyramid
-	for (x, y, window) in sliding_window(resized, stepSize=32, windowSize=(winW, winH)):
-		# if the window does not meet our desired window size, ignore it
-		if window.shape[0] != winH or window.shape[1] != winW:
-			continue
+for each in image:
+	for resized in pyramid(each, scale=1.5):
+		# loop over the sliding window for each layer of the pyramid
+		for (x, y, window) in sliding_window(resized, stepSize=32, windowSize=(winW, winH)):
+			# if the window does not meet our desired window size, ignore it
+			if window.shape[0] != winH or window.shape[1] != winW:
+				continue
 
-		# THIS IS WHERE YOU WOULD PROCESS YOUR WINDOW, SUCH AS APPLYING A
-		# MACHINE LEARNING CLASSIFIER TO CLASSIFY THE CONTENTS OF THE
-		# WINDOW
+			# THIS IS WHERE YOU WOULD PROCESS YOUR WINDOW, SUCH AS APPLYING A
+			# MACHINE LEARNING CLASSIFIER TO CLASSIFY THE CONTENTS OF THE
+			# WINDOW
 
-		# since we do not have a classifier, we'll just draw the window
-		clone = resized.copy()
-		cv2.rectangle(clone, (x, y), (x + winW, y + winH), (0, 255, 0), 2)
-		cv2.imshow("Window", clone)
-		cv2.waitKey(1)
-		time.sleep(0.025)
+			# since we do not have a classifier, we'll just draw the window
+			clone = resized.copy()
+			cv2.rectangle(clone, (x, y), (x + winW, y + winH), (0, 255, 0), 2)
+			cv2.imshow("Window", clone)
+			cv2.waitKey(1)
+			time.sleep(0.025)
